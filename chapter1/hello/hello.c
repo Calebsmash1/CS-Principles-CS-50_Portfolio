454bff3924963ae2e7c03bbf838c5b69448d031e
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-09-30 14:49:59 +0000

    Fri, Sep 30, 2022, 2:49 PM +00:00

--- chapter1/hello/hello.c
+++ chapter1/hello/hello.c
@@ -3,7 +3,9 @@
 
 int main(void)
 {
+    //Prompt the user their name then saves it to a variable called name
     string name = get_string("Name: ");
 
+    //Say hello to user using their name
     printf("hello, %s\n", name);
 }
