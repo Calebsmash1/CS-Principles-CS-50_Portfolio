a7d653788059826e92280a32d2cc0368acceded4
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-10-03 14:52:50 +0000

    Mon, Oct 3, 2022, 2:52 PM +00:00

--- chapter1/variables/test0.c
+++ chapter1/variables/test0.c
@@ -13,8 +13,8 @@ int main(void)
     //Declare an integer variable called x and assign 7 to it
     int x;
 
-    // Declare an integer variable called y and assign 3 to it
-    int y = 3;
+    // Declare an integer variable called y and assign x + 1 to it
+    int y = x + 1;
 
     //Print the current value
     printf("The value of x is %i and the value of y is %i\n, x, y);
