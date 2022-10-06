ec411dc319830853b3644bd370542c23c41497da
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-10-06 14:36:03 +0000

    Thu, Oct 6, 2022, 2:36 PM +00:00

--- chapter1/operators/power.c
+++ chapter1/operators/power.c
@@ -4,12 +4,15 @@
 
 #include <cs50.h>
 #include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
     // Prompt the user for a number
-    double number = get_double("Give ma a number, please: ");
+    double number = get_double("Give me a number, please: ");
 
     // Prompt the user for a power
     double p = get_double("Give a power, please: ");
+
+    printf("%f raised to the power of %f is %f\n", number, p, pow(number, p));
 }
