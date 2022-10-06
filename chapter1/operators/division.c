01b013ecc07d743271d1de41b8bf42921e532d91
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-10-06 14:21:57 +0000

    Thu, Oct 6, 2022, 2:21 PM +00:00

---
+++ chapter1/operators/division.c
@@ -0,0 +1,15 @@
+/**
+ * Make division actually happen
+ */
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    float first = get_float("Number: ");
+
+    float second = get_float("Another number: ");
+
+    
+}
