06c3595f729fda8b1190da4555fcad21946de12c
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-10-06 14:25:13 +0000

    Thu, Oct 6, 2022, 2:25 PM +00:00

--- chapter1/operators/division.c
+++ chapter1/operators/division.c
@@ -7,9 +7,13 @@
 
 int main(void)
 {
+    // Prompts user for a number and saves it
     float first = get_float("Number: ");
 
+    // Prompts the user for another number and saves it
     float second = get_float("Another number: ");
 
-    
+    // Divide these two numbers and print the value
+    printf("%f divided by %f is %f\n", first, second, first / second);
+
 }
