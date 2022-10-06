d7b1346e6b1f69973503de818f05cd65000c30c5
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-10-06 14:31:41 +0000

    Thu, Oct 6, 2022, 2:31 PM +00:00

---
+++ chapter1/operators/power.c
@@ -0,0 +1,15 @@
+/**
+ * Raise a number to a power
+ */
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Prompt the user for a number
+    double number = get_double("Give ma a number, please: ");
+
+    // Prompt the user for a power
+    double p = get_double("Give a power, please: ");
+}
