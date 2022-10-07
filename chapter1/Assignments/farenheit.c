18a22449e0c2e23573616417d0619e8728a06adc
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-10-07 13:19:20 +0000

    Fri, Oct 7, 2022, 1:19 PM +00:00

--- chapter1/Assignments/farenheit.c
+++ chapter1/Assignments/farenheit.c
@@ -11,8 +11,6 @@ int main(void)
     // Prompts the user for a temperature in Celsius
     int number1 = get_int("Integer: ");
 
-    // Converts and prints user input into Farenheit
-    printf("%i plus %i is %i\n", number1, number2, number1 + number2);
 
 
 }
