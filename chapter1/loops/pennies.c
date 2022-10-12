a391db278ff2ac37e12a7cc8d1f8fa990fa69ddf
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-10-12 14:58:26 +0000

    Wed, Oct 12, 2022, 2:58 PM +00:00

---
+++ chapter1/loops/pennies.c
@@ -0,0 +1,10 @@
+// Calculate the number of pennies someone would have
+// If their change doubled every day for a month
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Get 
+}
