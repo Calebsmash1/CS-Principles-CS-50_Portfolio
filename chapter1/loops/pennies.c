a15d02470bdbe0c65a96d6147ab26ed2466f222d
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-10-13 14:18:09 +0000

    Thu, Oct 13, 2022, 2:18 PM +00:00

--- chapter1/loops/pennies.c
+++ chapter1/loops/pennies.c
@@ -7,6 +7,12 @@
 int main(void)
 {
     // Get starting change in pennies
+    int pennies = get_int("Number of pennies to start: ");
 
-    // Get
+    // Get the number of days in the month
+    int days = get_int("Number of days in month: ");
+
+    // Double the number of pennies for each of the days
+
+    // Print the number of pennies after a month
 }
