e1408f01173e7cabcb96dca157a0af008660d3c7
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-10-13 23:30:04 +0000

    Thu, Oct 13, 2022, 11:30 PM +00:00

--- chapter1/cash/cash.c
+++ chapter1/cash/cash.c
@@ -11,14 +11,18 @@
 int main(void)
 {
     // Asks user for the amount of change owed
-    float change = get_float("Change owed:");
+    float change;
+     do{
+            change = get_float("Change owed: ");
+    }
+    while (change < 1);
 
     // Finds the least amount of coins that should be
     // returned to the user
-    
+
 
     //Returns change to user
-    printf("%0.2f\n", finalchange);
+    //printf("%0.2f\n", finalchange);
 
     /**
      *
