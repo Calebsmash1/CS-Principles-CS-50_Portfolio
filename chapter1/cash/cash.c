c4fdeb34472a8025550e53d6519032256e4d27b6
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-10-14 14:15:47 +0000

    Fri, Oct 14, 2022, 2:15 PM +00:00

--- chapter1/cash/cash.c
+++ chapter1/cash/cash.c
@@ -19,16 +19,16 @@ int main(void)
     while (change < 0);
 
     // Establishes available change options
-    int dollar = (1);
-    float quarter = (0.25);
-    float dime = (0.10);
-    float nickel = (0.05);
-    float penny = (0.01);
+    //int dollar = (1);
+    //float quarter = (0.25);
+    //float dime = (0.10);
+    //float nickel = (0.05);
+    //float penny = (0.01);
     // Finds the least amount of coins that should be
     // returned to the user using floor decision
     // and displays it
 
-    printf("%0.0i\n", change / dollar);
+    printf("%0.0f\n", change / 0.25);
 
     //Returns change to user
     // printf("%0.2f\n", finalchange);
