17bf036ef701b07648dfa32a611b657932fbdc50
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-10-14 00:03:00 +0000

    Fri, Oct 14, 2022, 12:03 AM +00:00

--- chapter1/cash/cash.c
+++ chapter1/cash/cash.c
@@ -20,8 +20,8 @@ int main(void)
 
     // Finds the least amount of coins that should be
     // returned to the user using floor decision
-
-    finalchange = change --
+    float finalchange;
+    finalchange = change / 1 / 0.25 / 0.10 / 0.5 / 0.1
 
     //Returns change to user
     printf("%0.2f\n", finalchange);
