6f0f906c1fba32097d9f25fcf82cba415fb8720b
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-10-14 14:33:33 +0000

    Fri, Oct 14, 2022, 2:33 PM +00:00

--- chapter1/cash/cash.c
+++ chapter1/cash/cash.c
@@ -22,6 +22,26 @@ int main(void)
         change = (change - 1);
     }
     while (change < 1);
+    do
+    {
+        change = (change - 0.25);
+    }
+    while (change < 0.25);
+    do
+    {
+        change = (change - 0.10);
+    }
+    while (change < 0.10);
+    do
+    {
+        change = (change - 0.05);
+    }
+    while (change < 0.05);
+    do
+    {
+        change = (change - 0.01);
+    }
+    while (change < 0.01);
     // Finds the least amount of coins that should be
     // returned to the user using floor decision
     // and displays it
