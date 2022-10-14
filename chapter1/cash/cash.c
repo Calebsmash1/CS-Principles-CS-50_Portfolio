8d2e40990e3be41e6a8a8a63a423285e324253fc
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-10-14 13:32:48 +0000

    Fri, Oct 14, 2022, 1:32 PM +00:00

--- chapter1/cash/cash.c
+++ chapter1/cash/cash.c
@@ -28,7 +28,7 @@ int main(void)
     // returned to the user using floor decision
     // and displays it
 
-    printf("%0.0f\n", change / dollar);
+    printf("%0.0f\n", change / quarter);
 
     //Returns change to user
     // printf("%0.2f\n", finalchange);
