a497a8291a09d68e82c3ce4241561dc14cc87558
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-10-14 14:48:58 +0000

    Fri, Oct 14, 2022, 2:48 PM +00:00

--- chapter1/cash/cash.c
+++ chapter1/cash/cash.c
@@ -17,29 +17,31 @@ int main(void)
         change = get_float("Change owed: ");
     }
     while (change < 0);
-    int finalchange = 0;
+    int finalchange = 1;
     do
     {
         finalchange = (finalchange + 1);
-        change = (change / 0.25);
+        change = (change - 0.25);
     }
     while (change >= 0.25);
     do
     {
         finalchange = (change + 1);
+        change = (change - 0.10);
     }
     while (change >= 0.10);
     do
     {
         finalchange = (finalchange + 1);
+        change = (change - 0.05);
     }
     while (change >= 0.05);
     do
     {
-        finalchange = (finalchange + 1);
+        finalchange = (finalchange + change);
     }
     while (change >= 0.01);
 
     // Returns change to user
-    printf("%0.0f\n", change);
+    printf("%0.0i\n", finalchange);
 }
