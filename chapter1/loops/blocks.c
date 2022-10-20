a62d26d4b34d82db5582e0681466a0789a599795
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-10-20 14:41:14 +0000

    Thu, Oct 20, 2022, 2:41 PM +00:00

---
+++ chapter1/loops/blocks.c
@@ -0,0 +1,18 @@
+// Blocks stacked
+#include <cs50.h>
+#include <stdio.h>
+
+int main (void)
+{
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height <=0);
+
+    for (int i = 0; i < height; i++)
+    {
+        printf("#\n");
+    }
+}
