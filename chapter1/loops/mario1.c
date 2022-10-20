6efbae484f4f11cd1467b8f93130ba771aedfc77
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-10-20 14:34:58 +0000

    Thu, Oct 20, 2022, 2:34 PM +00:00

--- chapter1/loops/mario1.c
+++ chapter1/loops/mario1.c
@@ -6,7 +6,12 @@
 int main (void)
 {
     // Get the number of ? that the user wants
-    int q = get_int("Number: ");
+    int q;
+    do
+    {
+        q = get_int("Number: ");
+    }
+    while (q <= 0);
 
     // For each question mark
     for (int i = 0; i < q; i++)
