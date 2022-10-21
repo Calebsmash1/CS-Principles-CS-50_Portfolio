7ee6f664201cca95c3a3377569b11439db27427f
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-10-21 14:39:12 +0000

    Fri, Oct 21, 2022, 2:39 PM +00:00

--- chapter1/mario-less/mario.c
+++ chapter1/mario-less/mario.c
@@ -3,10 +3,16 @@
 
 int main(void)
 {
+    // Prompts the user for height of pyramid
     int height;
     do
     {
         height = get_int("Height: ");
     }
     while (height <=0);
+
+    for (int i = 0; i < height; i++)
+    {
+        printf("#\n");
+    }
 }
