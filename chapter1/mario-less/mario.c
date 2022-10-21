b79171ac925d6a2c6f882b6d9c26311d485eb74f
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-10-21 14:44:25 +0000

    Fri, Oct 21, 2022, 2:44 PM +00:00

--- chapter1/mario-less/mario.c
+++ chapter1/mario-less/mario.c
@@ -11,6 +11,7 @@ int main(void)
     }
     while (height <=0);
 
+    // Calculates how many spaces should exist and prints the triangle
     for (int i = 0; i < height; i++)
     {
         printf("#\n");
