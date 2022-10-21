99df0fed5027cb82a12087d0d2728f3c29deb8b9
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-10-21 14:53:49 +0000

    Fri, Oct 21, 2022, 2:53 PM +00:00

--- chapter1/mario-less/mario.c
+++ chapter1/mario-less/mario.c
@@ -5,16 +5,21 @@ int main(void)
 {
     // Prompts the user for height of pyramid
     int height;
+    int length;
     do
     {
         height = get_int("Height: ");
     }
     while (height <=0);
+    length = height;
 
     // Calculates how many spaces should exist and prints the triangle
     for (int h = 0; h < height; h++)
     {
-        for (int l = 0; l < lenght; l++)
-        printf("#\n");
+        for (int l = 0; l < length; l++)
+        {
+
+        }
+        printf("*#\n");
     }
 }
