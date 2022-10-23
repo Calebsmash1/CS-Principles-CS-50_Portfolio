e3bf59c464890eb62290929d9f055e58963f72bc
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-10-23 19:35:22 +0000

    Sun, Oct 23, 2022, 7:35 PM +00:00

---
+++ chapter1/loops/mario/t.c
@@ -0,0 +1,31 @@
+
+
+// Print a triangle of blocks
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Get a height from the user
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height <= 0);
+
+    // For each row
+    for (int row = 0; row < height; row++)
+    {
+        // For each column
+        for (int col = -1; col < row; col++)
+        {
+            for(int blank = 0; )
+            // Print a block
+            printf("#");
+        }
+
+        // Print a new line to prepare for next row
+        printf("\n");
+    }
+}
