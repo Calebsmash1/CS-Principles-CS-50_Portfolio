3662e085e6961ff91485c07a21fb0604bb177987
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-10-24 14:27:24 +0000

    Mon, Oct 24, 2022, 2:27 PM +00:00

--- chapter1/loops/mario/tri.c
+++ chapter1/loops/mario/tri.c
@@ -10,7 +10,7 @@ int main(void)
     {
         height = get_int("Height: ");
     }
-    while (height <= 0);
+    while (height <= 0 || height >9);
 
     // For each row
     for (int row = 0; row < height; row++)
