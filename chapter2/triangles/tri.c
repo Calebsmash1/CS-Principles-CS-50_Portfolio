1682a86e78aa7aed43419b38d2c6111ce97a7996
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-10-31 14:47:23 +0000

    Mon, Oct 31, 2022, 2:47 PM +00:00

--- chapter2/triangles/tri.c
+++ chapter2/triangles/tri.c
@@ -23,7 +23,7 @@ bool valid_triangle(float x, float y, float z)
 {
 
     // TODO: Check for all positive sides, return false if not
-    if()
+    if(x)
 
     // TODO: Check that sum of any two sides greater than third, return false if not
 
