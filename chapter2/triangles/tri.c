3f63ec91537e3e3ee0063b5ce38b08edf106abdb
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-11-01 00:48:54 +0000

    Tue, Nov 1, 2022, 12:48 AM +00:00

--- chapter2/triangles/tri.c
+++ chapter2/triangles/tri.c
@@ -15,13 +15,14 @@ int main(void)
 
     // TODO: Determine if a valid triangle by calling valid_triangle
     // then print result "Valid!" or "Invalid!"
-    printf("%d!\n");
+    printf("%i!\n");
 }
 
 // Function that checks to see if a valid triangle
 bool valid_triangle(float x, float y, float z)
 {
-
+    int Valid = true
+    int Invalid = false
     // Check for all positive sides, return false if not
     if(x || y || z <= 0)
     {
