c6ff6c720c8a9b0656437127141377315e2ece0d
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-11-01 01:03:01 +0000

    Tue, Nov 1, 2022, 1:03 AM +00:00

--- chapter2/triangles/tri.c
+++ chapter2/triangles/tri.c
@@ -15,11 +15,11 @@ int main(void)
 
     // TODO: Determine if a valid triangle by calling valid_triangle
     // then print result "Valid!" or "Invalid!"
-    if (return false)
+    if (valid_triangle == false)
     {
         printf("Invalid!")
     }
-    else
+    if (return)
     (
         printf("Valid!")
     )
