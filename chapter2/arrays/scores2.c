2a131a996f7f9f46e650b477557738d14cb9617e
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-11-02 14:56:18 +0000

    Wed, Nov 2, 2022, 2:56 PM +00:00

--- chapter2/arrays/scores2.c
+++ chapter2/arrays/scores2.c
@@ -3,7 +3,6 @@
 #include <cs50.h>
 #include <stdio.h>
 
-
 int main(void)
 {
     // Let the user decide how many scores to enter
@@ -14,9 +13,8 @@ int main(void)
     for (int i = 0; i < n; i++)
     {
         scores[i] = get_int("Score: ");
-
     }
 
     // Print the average
-    printf("Average: %0.3f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
+    printf("Average: %0.3f\n", (scores[0] + scores[1] + scores[2]) / (float) n);
 }
