8490c809db576a1a621c0cabd3d73e172bc0f76c
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-11-02 14:28:52 +0000

    Wed, Nov 2, 2022, 2:28 PM +00:00

--- chapter2/arrays/countdown.c
+++ chapter2/arrays/countdown.c
@@ -6,5 +6,11 @@
 int main(void)
 {
     // Initialize an array to hold our counting values
-    
+    int countdown[10] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+
+    // Print out each element in the array
+    for(int i = 0; i < 10; i++)
+    {
+        
+    }
 }
