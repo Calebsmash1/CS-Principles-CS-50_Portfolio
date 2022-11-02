5c9c2d367edac2167793e9875a10b1f81913bad4
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-11-02 14:48:53 +0000

    Wed, Nov 2, 2022, 2:48 PM +00:00

--- chapter2/arrays/scores1.c
+++ chapter2/arrays/scores1.c
@@ -1,17 +1,16 @@
-// Average three numbers (hardcoded) using an array
+// Average three numbers using an array
 
 #include <cs50.h>
 #include <stdio.h>
-
 
 
 int main(void)
 {
     // Scores
     int scores[3];
-    scores[0] = 63;
-    scores[1] = 42;
-    scores[2] = 98;
+    scores[0] = get_int("Score: ");
+    scores[1] = get_int("Score: ");
+    scores[2] = get_int("Score: ");
 
     // Print the average
     printf("Average: %0.3f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
