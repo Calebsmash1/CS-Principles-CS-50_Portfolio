5c0824481416ea9c24d97663a5cfcbb472d8b4ac
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-11-02 14:44:29 +0000

    Wed, Nov 2, 2022, 2:44 PM +00:00

--- chapter2/arrays/scores0.c
+++ chapter2/arrays/scores0.c
@@ -9,4 +9,10 @@ int main(void)
 {
     // Scores
     int scores[3];
+    scores[0] = 63;
+    scores[1] = 42;
+    scores[2] = 99;
+
+    // Print the average
+    printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3);
 }
