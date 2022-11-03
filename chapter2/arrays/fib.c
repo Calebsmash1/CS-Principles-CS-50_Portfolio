1e6997d7c6733860f9bd50b870a2eb2decdeda77
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-11-03 14:42:32 +0000

    Thu, Nov 3, 2022, 2:42 PM +00:00

---
+++ chapter2/arrays/fib.c
@@ -0,0 +1,20 @@
+// Display the first 25 Fibo numbers
+
+#include <cs50.h>
+#include <stdio.h>
+
+#define N 25
+
+int main(Void)
+{
+    // Declare an array and store the first two fibo terms
+    int fibo[N]
+    fibo[0] = 1;
+    fibo[1] = 1;
+
+    // Loop to fill in the rest
+    for (int i = 2; i < N; i++)
+    {
+        fibo[i] = 
+    }
+}
