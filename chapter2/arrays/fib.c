125f0a7bcfc6a149a788a37d6217453697af159f
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-11-04 16:07:27 +0000

    Fri, Nov 4, 2022, 4:07 PM +00:00

--- chapter2/arrays/fib.c
+++ chapter2/arrays/fib.c
@@ -3,11 +3,12 @@
 #include <cs50.h>
 #include <stdio.h>
 
-#define N 25
+//#define N 25
 
 int main(void)
 {
     // Declare an array and store the first two fibo terms
+    int N = get_int("How many courses do you have homework in? ");
     int fibo[N];
     fibo[0] = 1;
     fibo[1] = 1;
