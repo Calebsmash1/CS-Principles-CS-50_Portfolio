9673fa85085849fbb4f621b50188a99834d1f516
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-11-05 00:59:21 +0000

    Sat, Nov 5, 2022, 12:59 AM +00:00

--- chapter2/homework/homework.c
+++ chapter2/homework/homework.c
@@ -28,6 +28,7 @@ int main(void)
     int r = rand() % n;
 
     // Print out a random course number with index r
+    // TODO: Change to print subject name instead of locker number
     printf("%d, I choose you!\n", rand() % n);
 
 }
