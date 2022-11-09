000809d1a00d8735cde75f0103036d08e2243bb9
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-11-09 15:52:19 +0000

    Wed, Nov 9, 2022, 3:52 PM +00:00

--- chapter2/initials/initials.c
+++ chapter2/initials/initials.c
@@ -11,7 +11,7 @@ int main(void)
     string s = get_string("Full name: ");
 
     // Prints first initial
-    printf("%c", s[0]);
+    printf("%c", toupper(s[0]));
 
     // Print the character inside the locker after a space as a capital.
     for (int i = 1; i < strlen(s); i++)
