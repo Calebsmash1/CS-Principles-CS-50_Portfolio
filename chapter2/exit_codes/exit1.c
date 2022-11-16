bef6865e2ef520aab1d923f987cf118489169ef3
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-11-16 15:40:21 +0000

    Wed, Nov 16, 2022, 3:40 PM +00:00

---
+++ chapter2/exit_codes/exit1.c
@@ -0,0 +1,30 @@
+/**
+ * Count the number of letters in the user's first name
+*/
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+    // Check to see if the requirements have been met
+    // Essentially ensure "./exit1 <first name>"
+    if (argc != 2)
+    {
+        // Print a usage error
+        printf("Usage: ./exit1 <first name>\n");
+
+        // Return an int for main, program stops running
+        return 1;
+    }
+
+    // Tally the number of characters in their first name
+    int tally = 0;
+    for (int i = 0; i < )
+    {
+
+    }
+
+    // Return a success for main
+    return 0;
+}
