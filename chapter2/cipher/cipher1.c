b4c31dac0974d6cd9e5b70b37d93ace4b0c7a320
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-11-17 16:52:16 +0000

    Thu, Nov 17, 2022, 4:52 PM +00:00

--- chapter2/cipher/cipher1.c
+++ chapter2/cipher/cipher1.c
@@ -1,4 +1,4 @@
-// De-cipher a message with 5 characters
+// Cipher a message with 5 characters
 
 #include <cs50.h>
 #include <stdio.h>
@@ -6,16 +6,16 @@
 
 int main(int argc, string argv[])
 {
-    string message = "[tnqf&";
+    string message = "Voila!";
 
-    // De-cipher the message
-    printf("The hidden message is: ");
+    // Cipher the message
+    printf("The cipher message is: ");
 
     // For each character in the message
     for (int i = 0; i < strlen(message); i++)
     {
         // Go back 5 ASCII characters
-        printf("%c", message[i] - 5);
+        printf("%c", message[i] + 5);
     }
     // New line
     printf("\n");
--- chapter2/cipher/cipher2.c
+++ chapter2/cipher/cipher2.c
@@ -1,4 +1,4 @@
-// De-cipher a message with 5 characters
+// Cipher a message with 5 characters with "wrapping"
 
 #include <cs50.h>
 #include <stdio.h>
@@ -6,16 +6,16 @@
 
 int main(int argc, string argv[])
 {
-    string message = "[tnqf&";
+    string message = "Voila!";
 
     // De-cipher the message
-    printf("The hidden message is: ");
+    printf("The cipher message is: ");
 
     // For each character in the message
     for (int i = 0; i < strlen(message); i++)
     {
-        // Go back 5 ASCII characters
-        printf("%c", message[i] - 5);
+        // Go back 5 ASCII characters with "wrapping" (Only works for upper case)
+        printf("%c", (message[i] + 5 - 'a') % 26 + 'a');
     }
     // New line
     printf("\n");
