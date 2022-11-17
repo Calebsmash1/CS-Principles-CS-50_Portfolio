8ce7456cf27c776ff3667d47aecacaa0b0814882
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-11-17 15:27:24 +0000

    Thu, Nov 17, 2022, 3:27 PM +00:00

---
+++ chapter2/cipher/cipher0.c
@@ -0,0 +1,16 @@
+// De-cipher a message with 5 characters
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+    string message = "[tnqf&";
+
+    // De-cipher the message
+    printf("The hidden message is: ");
+
+    // For each character in the message
+    
+
+}
