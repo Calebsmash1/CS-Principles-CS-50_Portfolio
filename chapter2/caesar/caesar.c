a6c38b6710f289a445fd5b688b2081f700b5f9a4
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-11-18 15:39:52 +0000

    Fri, Nov 18, 2022, 3:39 PM +00:00

--- chapter2/caesar/caesar.c
+++ chapter2/caesar/caesar.c
@@ -16,7 +16,7 @@ int main(int argc, string argv[])
     // TODO: Prompt user for plaintext
         // Check to see if the requirements have been met
     // Essentially ensure "./exit <first name>"
-    if (argc != 2)
+    if (argc = 0)
     {
         // Print a usage error
         printf("Usage: ./exit <first name>\n");
