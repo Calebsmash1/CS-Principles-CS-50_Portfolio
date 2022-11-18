945083f02baf72e9b36b8253d095bd921bcab318
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-11-18 15:22:11 +0000

    Fri, Nov 18, 2022, 3:22 PM +00:00

---
+++ chapter2/Caesar/caesar.c
@@ -0,0 +1,20 @@
+// Cipher a message 
+
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    // TODO: Program was run with just one command-line argument
+
+    // TODO: Every character in argv[1] is a digit
+
+    // TODO: Convert argv[1] from a `string` to an `int`
+
+    // TODO: Prompt user for plaintext
+
+    // TODO: For each character in the plaintext:
+
+        // TODO: Rotate the character if it's a letter
+}
