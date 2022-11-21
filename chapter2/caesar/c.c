7e8be00d212dd48bb83efe7ad9d3000e223934c8
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-11-21 23:08:01 +0000

    Mon, Nov 21, 2022, 11:08 PM +00:00

--- chapter2/caesar/c.c
+++ chapter2/caesar/c.c
@@ -1,5 +1,21 @@
-
+// Ciphers to selected amount
 
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
+
+int main(void)
+{
+    // TODO: Make sure program was run with just one command-line argument
+
+    // TODO: Make sure every character in argv[1] is a digit
+
+    // TODO: Convert argv[1] from a `string` to an `int`
+
+    // TODO: Prompt user for plaintext
+
+    // TODO: For each character in the plaintext:
+
+        // TODO: Rotate the character if it's a letter
+}
