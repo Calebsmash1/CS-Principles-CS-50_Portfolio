bafdf81d908bfc565453c967807f998df9c73abe
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-11-22 00:48:19 +0000

    Tue, Nov 22, 2022, 12:48 AM +00:00

--- chapter2/caesar/c.c
+++ chapter2/caesar/c.c
@@ -8,6 +8,7 @@
 
 int main(int argc, string argv[])
 {
+    // Make sure program was run with just one command-line argument
     if (argc != 2)
     {
         // Print a usage error
@@ -17,31 +18,31 @@ int main(int argc, string argv[])
         return 1;
     }
 
-    // TODO: Make sure program was run with just one command-line argument (2nd to Last)
-
-    // TODO: Make sure every character in argv[1] is a digit (Last)
-
-    // TODO: Convert argv[1] from a `string` to an `int`
-
-    int key = atoi (argv[1]);
-
-    // TODO: Prompt user for plaintext
-    string message = get_string("plaintext: ");
-
-    // TODO: For each character in the plaintext:
-    for (int i = 0; i < strlen(message); i++)
+    // Make sure every character in argv[1] is a digit
+    if (isdigit(argv[1]))
     {
-        // TODO: Rotate the character if it's a letter
-        if (isalpha(message[i]))
+        // Convert argv[1] from a `string` to an `int`
+        int key = atoi (argv[1]);
+
+        // Prompt user for plaintext
+        string message = get_string("plaintext: ");
+
+        // For each character in the plaintext:
+        for (int i = 0; i < strlen(message); i++)
         {
-            printf("%c", (message[i] + key - 'a') % 26 + 'a');
+            // Rotate the character if it's a letter
+            if (isalpha(message[i]))
+            {
+                // Prints ciphered message letter by letter
+                printf("%c", (message[i] + key - 'a') % 26 + 'a');
+            }
         }
+
+        // Prints new line
+        printf("\n");
+
+        // Returns 0 to end program
+        return 0;
     }
-
-    // Prints out hidden message
-
-    printf("\n");
-
-    // Returns 0 to end program
-    return 0;
+    
 }
