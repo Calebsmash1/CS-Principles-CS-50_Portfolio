3301dee97928c3eb77170eeace73ebb3d875c76d
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-11-22 00:31:52 +0000

    Tue, Nov 22, 2022, 12:31 AM +00:00

--- chapter2/caesar/c.c
+++ chapter2/caesar/c.c
@@ -8,7 +8,7 @@
 
 int main(int argc, string argv[])
 {
-    if (argc == 1; isdigit(arg[1]))
+    if (argc >= 1)
     {
         // Print a usage error
         printf("Usage: ./caesar <message>\n");
