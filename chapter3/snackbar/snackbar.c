7e19b31f19cdd9cf278987889bae6f7a2f2c89dd
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-12-08 15:32:00 +0000

    Thu, Dec 8, 2022, 3:32 PM +00:00

--- chapter3/snackbar/snackbar.c
+++ chapter3/snackbar/snackbar.c
@@ -73,37 +73,35 @@ int main(void)
 // Add items to the menu array
 void add_items(void)
 {
+    menu[0].item = "Burger";
+    menu[0].price = 9.5;
 
-    menu[1].item = "Burger"
-    menu[1].price = 9.5
+    menu[1].item = "Vegan Burger";
+    menu[1].price = 11;
 
-    menu[2].item = "Vegan Burger
-    menu[2].price = 11
+    menu[2].item = "Hot Dog";
+    menu[2].price = 5;
 
-    menu[3].item = "Hot Dog"
-    menu[3].price = 5
+    menu[3].item = "Cheese Dog";
+    menu[3].price = 7;
 
-    menu[4].item = "Cheese Dog
-    menu[4].price = 7
+    menu[4].item = "Fries";
+    menu[4].price = 5;
 
-    menu[5].item = "Fries"
-    menu[5].price = 5
+    menu[5].item = "Cheese Fries";
+    menu[5].price = 6;
 
-    menu[6].item = "Cheese Fries"
-    menu[6].price = 6
+    menu[6].item = "Cold Pressed Juice";
+    menu[6].price = 7;
 
-    menu[7].item = "Cold Pressed Juice"
-    menu[7].price = 7
+    menu[7].item = "Cold Brew";
+    menu[7].price = 3;
 
-    menu[8].item = "Cold Brew"
-    menu[8].price = 3
+    menu[8].item = "Water";
+    menu[8].price = 2;
 
-    menu[9].item = "Water"
-    menu[9].price = 2
-
-    menu[10].item = "Soda"
-    menu[10].price = 2
-
+    menu[9].item = "Soda";
+    menu[9].price = 2;
 
     return;
 }
@@ -111,5 +109,14 @@ void add_items(void)
 // Search through the menu array to find an item's cost
 float get_cost(string item)
 {
+    for (int i = 0; i < 5; i++)
+    {
+        if (strcasecmp(people[i].name  == 0)
+        {
+            printf("Found %s\n", people[i].number);
+            return 0;
+        }
+    }
+    
     return 0.0;
 }
