f00ba0ef7b0d15908ef009e57919bf63c6f9b253
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-12-12 18:04:21 +0000

    Mon, Dec 12, 2022, 6:04 PM +00:00

--- chapter3/plurality/plurality.c
+++ chapter3/plurality/plurality.c
@@ -70,10 +70,12 @@ bool vote(string name)
     // Look for name in candidates
     for (int i = 0; i < 10; i++)
     {
+        //If found
         if (strcasecmp(candidate[i].name  == 0)
         {
-            printf("Found %s\n", people[i].number);
-            return 0;
+            // Add 1 to votes
+
+            //Return true
         }
     }
         // If found
