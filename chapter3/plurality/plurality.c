baa511ed12df0f8a7e30550698b2e5c3a12b37c3
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2022-12-19 15:56:40 +0000

    Mon, Dec 19, 2022, 3:56 PM +00:00

--- chapter3/plurality/plurality.c
+++ chapter3/plurality/plurality.c
@@ -92,11 +92,10 @@ void print_winner(void)
 
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].votes voter_count > 2)
+        if (candidates[i].votes > voter_count / 2)
         {
         // Print out the candidate(s) with the largest number of votes
-        printf("%s", candidates[i].name);
-        return true;
+        printf("%s\n", candidates[i].name);
         }
     }
     return;
