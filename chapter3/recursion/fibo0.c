b2bd9042942c262b2ade9a36c601f274726c61e6
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-01-10 15:23:00 +0000

    Tue, Jan 10, 2023, 3:23 PM +00:00

--- chapter3/recursion/fibo0.c
+++ chapter3/recursion/fibo0.c
@@ -8,4 +8,18 @@
 int main(void)
 {
     // Print the first %i numvers in the Fibonacci series
+    printf("the first %i numbers in the Fibonacci series are: \n", N);
+    for (int i = 0; i < N; i++)
+    {
+        printf("%i ", fibo(i));
+    }
+    printf("\n");
+}
+
+/**
+ * Return the nth Fibonacci number
+*/
+int fibo(int n)
+{
+    
 }
