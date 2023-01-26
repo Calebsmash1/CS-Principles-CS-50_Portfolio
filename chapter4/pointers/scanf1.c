ef9ad92199b1750e4969955a9201db24a8410a60
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-01-26 15:42:58 +0000

    Thu, Jan 26, 2023, 3:42 PM +00:00

---
+++ chapter4/pointers/scanf1.c
@@ -0,0 +1,11 @@
+// Get a string from the user using scanf
+
+#include <stdio.h>
+
+int main(void)
+{
+    char s[4];
+    printf("s: ");
+    scanf("%s", s);
+    printf("The string is %s\n", s);
+}
