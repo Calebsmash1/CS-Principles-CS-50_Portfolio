f0a51f450cfd0d2e3f9048c677f6928234dcfe05
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-01-27 15:50:38 +0000

    Fri, Jan 27, 2023, 3:50 PM +00:00

---
+++ chapter4/file_pointers/printtext.c
@@ -0,0 +1,14 @@
+// Open a txt file and print to the screen
+
+#include <stdio.h>
+
+int main(void)
+{
+    // Open .txt file
+
+    // Check if opened successfully
+
+    // Read the .txt file
+
+    // Print every character
+}
