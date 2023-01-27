12d60904083ee9da1e9bbdbe2b3cafc557b5e742
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-01-27 15:39:20 +0000

    Fri, Jan 27, 2023, 3:39 PM +00:00

--- chapter4/file_pointers/phonebook.c
+++ chapter4/file_pointers/phonebook.c
@@ -1,2 +1,23 @@
 // Save names and numbers to a CSV file
 
+#include <stdio.h>
+
+int main(void)
+{
+    // Open a .csv file
+    FILE *fonebook = fopen("phonebook.csv", "w");
+    if(!fonebook)
+
+    // Check to see if file opened successfully
+    {
+        printf("Error loading file\n");
+        return 1;
+    }
+
+    // Get name and number
+
+
+    // Write to the file
+
+    // Close the file
+}
