25f9b26951b91afed64bdccbed277f65e6c3d8c8
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-01-27 15:41:26 +0000

    Fri, Jan 27, 2023, 3:41 PM +00:00

--- chapter4/file_pointers/phonebook.c
+++ chapter4/file_pointers/phonebook.c
@@ -1,5 +1,6 @@
 // Save names and numbers to a CSV file
 
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
@@ -15,7 +16,8 @@ int main(void)
     }
 
     // Get name and number
-
+    char *name = get_string("Name: ");
+    char *number = get_string("Number: ");
 
     // Write to the file
 
