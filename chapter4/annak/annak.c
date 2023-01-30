4c0686ef04be2ec5452614b8c56072bd56eae9e2
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-01-30 17:15:06 +0000

    Mon, Jan 30, 2023, 5:15 PM +00:00

--- chapter4/annak/annak.c
+++ chapter4/annak/annak.c
@@ -33,11 +33,11 @@ int main(int argc, char* argv[])
 
     // Read the contents of the source file one char (int!) at a time
     // Remember to use int so that the c != EOF comparison is reliable
-    for (char ch = fgetc(source); ch != EOF; ch = fgetc(dest))
+    for (char ch = fgetc(source); ch != EOF; char ar = fgetc(dest))
     {
         // Write the character to the destination file!
         // Hint* you will need to find a function that will print/put a character in the destination file.
-        fputc(ch, FILE *dest);
+        fputc(ch, ar *dest);
 
     }
 
