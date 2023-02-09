f2fc168fc50e8698e4910fd0ee8a54fca29b350f
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-02-09 15:57:13 +0000

    Thu, Feb 9, 2023, 3:57 PM +00:00

--- chapter5/intro/mario1.c
+++ chapter5/intro/mario1.c
@@ -3,6 +3,7 @@ from cs50 import get_int
 #Get the height from the user
 n = get_int("Height: ")
 
-#Print block vertically
+#Print block horizontally
 for i in range(n):
-    print("#")
+    print("#", end="")
+print()
