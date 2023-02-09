9ef38bda2ec663f1be04affd4c7538508b22fa94
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-02-09 16:57:21 +0000

    Thu, Feb 9, 2023, 4:57 PM +00:00

---
+++ chapter5/sentimental-mario-less/mario.py
@@ -0,0 +1,15 @@
+from cs50 import get_int
+
+#Get the height from the user
+n = get_int("Height: ")
+
+#Print block horizontally
+for i in range(n):
+    print("#", end="")
+print()
+
+#Print block vertically
+for i in range(n):
+    print("#")
+
+main()
