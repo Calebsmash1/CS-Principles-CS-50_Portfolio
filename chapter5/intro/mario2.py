eb29123cd5eaf1f3695c746f7b112195491f90ad
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-02-10 14:22:08 +0000

    Fri, Feb 10, 2023, 2:22 PM +00:00

--- chapter5/intro/mario2.py
+++ chapter5/intro/mario2.py
@@ -1,5 +1,6 @@
 from cs50 import get_int
 def main():
+    
     # Get the height from the user
     n = get_height()
 
@@ -11,7 +12,7 @@ def main():
 def get_height():
     while True:
         n = get_int("Height: ")
-        if n >= 0:
+        if n > 0:
             break
     return n
 
