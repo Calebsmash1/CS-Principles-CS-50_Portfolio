777b179a62a02f7c63a7f202c889621d49343635
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-02-10 14:24:30 +0000

    Fri, Feb 10, 2023, 2:24 PM +00:00

---
+++ chapter5/intro/mario3.py
@@ -0,0 +1,22 @@
+from cs50 import get_int
+def main():
+
+    # Get the height from the user
+    n = get_height()
+
+    # Print square
+    for i in range(n):
+        for j in range(n):
+            print("#", end="")
+        
+
+
+
+def get_height():
+    while True:
+        n = get_int("Height: ")
+        if n > 0:
+            break
+    return n
+
+main()
