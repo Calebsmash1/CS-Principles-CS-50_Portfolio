d660301f45c70e02dab0fe5fe378610ba7b1444b
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-02-13 15:54:56 +0000

    Mon, Feb 13, 2023, 3:54 PM +00:00

--- chapter5/sentimental-mario-less/mario.py
+++ chapter5/sentimental-mario-less/mario.py
@@ -1,23 +1,25 @@
 from cs50 import get_int
-def main():
 
-    # Get the height from the user
+
+def main():
+    # Get the height from the user (Calls function)
     n = get_height()
 
-    # Print square
+    # For each row
     for i in range(n):
+        # For each height
         for j in range(n):
+            # Prints space
             if (j < n - i - 1):
                 print(" ", end="")
             else:
                 print("#", end="")
         print()
 
-
 def get_height():
     while True:
         n = get_int("Height: ")
-        if n > 0:
+        if 0 < n < 10:
             break
     return n
 
