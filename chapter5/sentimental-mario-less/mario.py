b8743c1355faf71c5bd57f52d41185570a91bd84
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-02-13 15:51:36 +0000

    Mon, Feb 13, 2023, 3:51 PM +00:00

--- chapter5/sentimental-mario-less/mario.py
+++ chapter5/sentimental-mario-less/mario.py
@@ -7,9 +7,10 @@ def main():
     # Print square
     for i in range(n):
         for j in range(n):
-            if (j < n - 1):
+            if (j < n - i - 1):
                 print(" ", end="")
-            print("#", end="")
+            else:
+                print("#", end="")
         print()
 
 
