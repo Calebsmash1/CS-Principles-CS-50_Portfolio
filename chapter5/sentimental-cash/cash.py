f41e351949d939a24b17d67c17d9dee7134b3c10
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-02-14 15:23:21 +0000

    Tue, Feb 14, 2023, 3:23 PM +00:00

--- chapter5/sentimental-cash/cash.py
+++ chapter5/sentimental-cash/cash.py
@@ -3,3 +3,15 @@ from cs50 import get_int
 
 def main():
     # Asks how many cents the customer needs
+    c = get_cents()
+
+    # Checks how many quarters are owed
+    c = c - quarters * 25;
+
+
+def get_cents():
+    while True:
+        c = get_int("Cents? ")
+        if c > 0:
+            break
+    return c
