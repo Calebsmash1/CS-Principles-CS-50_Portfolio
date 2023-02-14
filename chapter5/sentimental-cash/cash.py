87aee2c8f676871dfed94fb220fe457b93fecc89
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-02-14 15:48:11 +0000

    Tue, Feb 14, 2023, 3:48 PM +00:00

--- chapter5/sentimental-cash/cash.py
+++ chapter5/sentimental-cash/cash.py
@@ -3,30 +3,50 @@ from cs50 import get_float
 
 def main():
     # Asks how many cents the customer needs
-    c = get_cents()
+    cents = get_cents()
 
     # Checks how many quarters are owed
-    c = c - quarters * 25
+    cents = cents - quarters * 25
 
     # Check how many dimes are owed
-    c = c - dimes * 10
+    cents = cents - dimes * 10
 
     # Check how many nickels are owed
-    c = c - nickels * 5
+    cents = cents - nickels * 5
 
     # Check how many pennies are owed
-    c = c - pennies
+    cents = cents - pennies
 
     # Adds them all
-    coins = quarters + dimes + nicke;s + pennies
+    coins = quarters + dimes + nickels + pennies
 
     # Gives back money
-    print(f)
+    print(f"{coins}")
 
 
 def get_cents():
     while True:
-        c = get_float("Cents? ")
-        if c > 0:
+        cents = get_float("Cents? ")
+        if cents > 0:
             break
-    return c
+    return cents
+
+
+def calculate_quarters(cents):
+    # Divide by 25 (value of a quarter) for rounding
+    return cents / 25
+
+
+def calculate_dimes(cents):
+    # Divide by 10 (value of a dime) for rounding
+    return cents / 10
+
+
+def calculate_nickels(cents):
+    # Divide by 5 (value of a nickel) for rounding
+    return cents / 5
+
+
+def calculate_pennies(cents):
+    # Returns the remainder (Total number of pennies)
+    return cents / 1
