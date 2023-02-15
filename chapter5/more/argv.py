69b26f4287ca66ef064f8c01080f2a19ebdc7bbe
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-02-15 15:58:50 +0000

    Wed, Feb 15, 2023, 3:58 PM +00:00

--- chapter5/more/argv.py
+++ chapter5/more/argv.py
@@ -1,6 +1,9 @@
 from sys import argv
 
-if len(argv) == 2:
-    print(f"Hello, {argv[1]}")
+if len(argv) >= 2:
+    print(f"Hello, ", end="")
+    for arg in argv[1:]:
+        print(f"{arg} ", end="")
+    print()
 else:
     print("Hello, world")
