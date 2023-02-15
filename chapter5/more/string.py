2fe32f33f443a2ef121ef320c854eb6e663d7c5f
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-02-15 15:41:52 +0000

    Wed, Feb 15, 2023, 3:41 PM +00:00

--- chapter5/more/string.py
+++ chapter5/more/string.py
@@ -1,6 +1,6 @@
-from cs50 import get_string
 
-before = get_string("Before : ")
+
+before = input("Before : ")
 print("After: ", end="")
 # Print each character of before 1 at a time
 for c in before:
