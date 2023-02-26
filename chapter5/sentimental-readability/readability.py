1d745fa25f6ec148aa8c4f184c1a267cb2391a03
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-02-26 23:54:48 +0000

    Sun, Feb 26, 2023, 11:54 PM +00:00

--- chapter5/sentimental-readability/readability.py
+++ chapter5/sentimental-readability/readability.py
@@ -3,13 +3,14 @@
 
 def main():
     # Asks for the text
-    text = input("Full name: ")
+    text = input("Text please: ")
 
-    # Checks how many letters are in text
+    # Checks how many letters are in the text
+
 
     # Finds average letters per 100 words
 
-    # Checks how many sentences are in text
+    # Checks how many sentences are in the text
 
     # Finds average sentences per 100 words
 
