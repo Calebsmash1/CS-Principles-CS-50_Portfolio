610b67b671e926e33dcd2c805016a809589b9205
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-02-27 02:36:15 +0000

    Mon, Feb 27, 2023, 2:36 AM +00:00

--- chapter5/sentimental-readability/readability.py
+++ chapter5/sentimental-readability/readability.py
@@ -15,7 +15,8 @@ def main():
     sentences = calculate_sentences(text)
 
     # Calculates reading level with Coleman-Liau index
-    final_level = round(0.0588 * (100.0 * letters / words) - 0.296 * (100.0 * sentences / words) - 15.8)
+    final_level = (0.0588 * (100.0 * letters / words) - 0.296 * (100.0 * sentences / words) - 15.8)
+    round(final_level)
 
     if final_level > 15:
         print("Grade 16+")
