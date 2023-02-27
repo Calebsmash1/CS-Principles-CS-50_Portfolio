91796cecc3239827ce80c74afed79213b9246a64
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-02-27 00:00:33 +0000

    Mon, Feb 27, 2023, 12:00 AM +00:00

--- chapter5/sentimental-readability/readability.py
+++ chapter5/sentimental-readability/readability.py
@@ -6,20 +6,28 @@ def main():
     text = input("Text please: ")
 
     # Checks how many letters are in the text
-    letters = calculate_letters()
+    letters = calculate_letters(text)
 
     # Finds average letters per 100 words
-    average_letters = calculate_average_letters()
+    average_letters = calculate_average_letters(text)
 
     # Checks how many sentences are in the text
-    sentences = calculate_sentences()
+    sentences = calculate_sentences(text)
 
     # Finds average sentences per 100 words
-    average_sentences = calculate_average_sentences()
+    average_sentences = calculate_average_sentences(text)
 
     # Calculates reading level with Coleman-Liau index
     index = calculate_index
     final_level = 0.0588 * average_letters - 0.296 * average_sentences - 15.8
+
+# Checks how many letters are in the text
+def calculate_letters():
+    while True:
+        # Starts a counter for the letters
+        letters = 1
+
+        # 
 
 
 main()
