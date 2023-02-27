6e8071be4aa4cb8e27c5ff9c616bfdc525f582c4
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-02-27 01:58:58 +0000

    Mon, Feb 27, 2023, 1:58 AM +00:00

--- chapter5/sentimental-readability/readability.py
+++ chapter5/sentimental-readability/readability.py
@@ -8,21 +8,19 @@ def main():
     # Checks how many letters are in the text
     letters = calculate_letters(text)
 
-    # Finds average letters per 100 words
-    #average_letters = calculate_average_letters(text)
+    # Checks how many words are in the text
+    words = calculate_words(text)
 
     # Checks how many sentences are in the text
     sentences = calculate_sentences(text)
 
-    # Finds average sentences per 100 words
-    #average_sentences = calculate_average_sentences(text)
-
     # Calculates reading level with Coleman-Liau index
-    #index = calculate_index
-    #final_level = 0.0588 * average_letters - 0.296 * average_sentences - 15.8
+    final_level = round(0.0588 * (100 * letters / words) - 0.296 * (100 * sentences / words) - 15.8)
 
     print(f"letters: {letters}")
     print(f"sentences: {sentences}")
+    print(f"words: {words}")
+    print(f"Grade {final_level}")
 
 # Checks how many letters are in the text
 def calculate_letters(text):
@@ -46,15 +44,15 @@ def calculate_sentences(text):
             sentences += 1
     return sentences
 
-#def calculate_words(text):
+def calculate_words(text):
     # Starts a counter for the words (Starts at 1)
-    #words = 1
-    #special_char = [" "]
+    words = 1
+    special_char = [" "]
 
-    #for char in text:
-        #if char in special_char:
-           #words += 1
-    #return words
+    for char in text:
+        if char in special_char:
+           words += 1
+    return words
 
 
 main()
