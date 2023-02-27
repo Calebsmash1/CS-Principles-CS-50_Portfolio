bc410d4ecdba3b9c08f13556cb49ef4214509679
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-02-27 15:36:05 +0000

    Mon, Feb 27, 2023, 3:36 PM +00:00

---
+++ chapter5/more/phonebook.py
@@ -0,0 +1,11 @@
+names = {
+    "Foster": "413-555-5555",
+    "Aiden": "800-123-4567"
+}
+
+name = input("Name: ")
+
+# Search for the name
+if name in names:
+    nubmer = names[name]
+    
--- chapter5/more/search2.py
+++
@@ -1,6 +0,0 @@
-names = {
-    "Foster": "413-555-5555",
-    "Aiden": "800-123-4567"
-}
-
-name = input("Name: ")
