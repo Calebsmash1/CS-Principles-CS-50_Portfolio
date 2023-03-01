f24f760fdbae541f8ab58685fe5057168b7af16e
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-03-01 15:20:44 +0000

    Wed, Mar 1, 2023, 3:20 PM +00:00

--- chapter5/csv/phonebook1.py
+++ chapter5/csv/phonebook1.py
@@ -4,13 +4,13 @@ from cs50 import get_string
 # Open phonebook.csv in "a"ppend mode
 file = open("phonebook.csv", "a")
 
-# Get numbers from the user
-name = input("Name: ")
-number = input("Number: ")
+# Get numbers from user
+name = get_string("Name: ")
+number = get_string("Number: ")
 
 # Write the name and number to the file
 writer = csv.writer(file)
-writer.writrow([name, number])
+writer.writerow([name, number])
 
 # Close the open file
 file.close()
--- chapter5/csv/phonebook2.py
+++ chapter5/csv/phonebook2.py
@@ -1,13 +1,21 @@
 import csv
-from cs50 import get_int
+from cs50 import get_string, get_int
 
-n = get_int("How many people?: ")
+# Get number of names to add to phonebook
+n = get_int("How many names would you like to add? ")
+print()
 
+# For each addition
 for i in range(n):
-    # Get numbers from the user
-    name = input("Name: ")
-    number = input("Number: ")
 
+    # Get numbers from user
+    name = get_string("Name: ")
+    number = get_string("Number: ")
+
+    # Add number to phonebook.csv
     with open("phonebook.csv", "a") as file:
         writer = csv.writer(file)
         writer.writerow((name, number))
+
+    # Print a new line
+    print()
