9224711ba56af98c1e62308941253a7eb4343631
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-03-01 15:34:48 +0000

    Wed, Mar 1, 2023, 3:34 PM +00:00

--- chapter5/csv/hogwarts.py
+++ chapter5/csv/hogwarts.py
@@ -1,9 +1,20 @@
 # Counts students in each house
 import csv
 
+# Dictionary to count the houses
+houses = {
+    "Gryffindor": 0,
+    "Hufflepuff': 0,
+    "Slytherin": 0,
+    "Ravenclaw": 0
+}
 # Open the csv
 with open("hogwarts.csv", "r") as house_list:
     # Read the rows and save as a variable called reader
     reader = csv.reader(house_list)
+    # Skip over the title row
     next(reader)
+
+    # Add 1 to the correct house for each row in house_list/reader
     for row in reader:
+        house = 
