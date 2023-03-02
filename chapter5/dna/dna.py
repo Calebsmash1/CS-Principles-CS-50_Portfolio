31e02b0cdec056fddd2da0abf6f674cee6229082
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-03-02 15:31:25 +0000

    Thu, Mar 2, 2023, 3:31 PM +00:00

--- chapter5/dna/dna.py
+++ chapter5/dna/dna.py
@@ -5,15 +5,18 @@ import sys
 def main():
 
     # TODO: Check for command-line usage
-    with open("small.csv", "r") as house_list:
 
-        # TODO: Read database file into a variable
+    # TODO: Read database file into a variable
+    people = []
+    with open(sys.argv[1] as db_file):
+        reader = csv.DictReader(db_file)
+        
 
-        # TODO: Read DNA sequence file into a variable
+    # TODO: Read DNA sequence file into a variable
 
-        # TODO: Find longest match of each STR in DNA sequence
+    # TODO: Find longest match of each STR in DNA sequence
 
-        # TODO: Check database for matching profiles
+    # TODO: Check database for matching profiles
 
     return
 
