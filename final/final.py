8ad82823371f72ff5ffdca3ca4e2c117077a7322
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-03-17 13:36:29 +0000

    Fri, Mar 17, 2023, 1:36 PM +00:00

--- final/final.py
+++ final/final.py
@@ -1,5 +1,18 @@
 
-x = input("x: ")
-y = input("y: ")
-added = int (x) + int (y)
-print(f"{added}")
+def main():
+    # Asks user how many strikers are trying out for team
+    prospects = input("How many strikers will be trying out?: ")
+
+    # Asks user how many spots there are
+    spots = input("How many strikers are you looking to recruit?: ")
+
+    # Asks user to fill out striker's stats
+    
+
+    # Calls OVR_calculator function to find which striker(s) makes it into the team
+
+
+def OVR_calculator()
+
+
+main()
