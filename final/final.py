96e1e69accd2ab2fb497b2df79efd2baf7a9c4dd
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-03-20 15:35:19 +0000

    Mon, Mar 20, 2023, 3:35 PM +00:00

--- final/final.py
+++ final/final.py
@@ -3,28 +3,11 @@
 # (Works under assumption that no two players have equal stats)
 
 def main():
-    # Asks user how many strikers are trying out for team
-    strikers = []
-    strikers = input("How many strikers are trying out?: ")
+    # Asks how many spots are there
+    spots = int input("How many spots are there?: ")
+    # Asks how many strikers are trying out
+    strikers = int input("How manys strikers are trying out?: ")
 
-    # Asks user how many spots there are
-    p = input("How many spots are there?: ")
-
-    # Dictionary of Strikers
-
-    # Dictionairy of Chosen Strikers
-
-    # Asks user to fill out striker's stats
-    shots
-
-
-    # Calls OVR_calculator function to find which striker(s) makes it into the team
-
-
-def OVR_calculator()
-
-
-def
-
-
-main()
+    if strikers <= spots:
+        print("Recruit them all!")
+        
