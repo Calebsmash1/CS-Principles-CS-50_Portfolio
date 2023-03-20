aaa75efcb5906fdd8ccb152d415cba86439b895f
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-03-20 15:38:10 +0000

    Mon, Mar 20, 2023, 3:38 PM +00:00

--- final/final.py
+++ final/final.py
@@ -2,13 +2,13 @@
 # soccer team's striker roster.
 # (Works under assumption that no two players have equal stats)
 
-def main():
-    # Asks how many spots are there
-    spots = input("How many spots are there?: ")
-    # Asks how many strikers are trying out
-    strikers = input("How manys strikers are trying out?: ")
 
-    if strikers <= spots:
-        print("Recruit them all!")
-        exit()
-    print("meh")
+# Asks how many spots are there
+spots = input("How many spots are there?: ")
+# Asks how many strikers are trying out
+strikers = input("How manys strikers are trying out?: ")
+
+if strikers <= spots:
+    print("Recruit them all!")
+    exit()
+print("meh")
