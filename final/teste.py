33a19c65c4bd35c0ebe7f99fd80056f0bc3a15b8
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-03-21 14:33:13 +0000

    Tue, Mar 21, 2023, 2:33 PM +00:00

---
+++ final/teste.py
@@ -0,0 +1,20 @@
+
+# def main():
+# Asks how many spots are there
+v = input("How many spots are there?: ")
+v = int(v)
+
+# Asks how many strikers are trying out
+s = input("How manys strikers are trying out?: ")
+s = int(s)
+if s <= v:
+    print("Recruit them all!")
+    exit()
+striker_name = []
+for i in range(s):
+    name = input("Striker name: ")
+    striker_name.append(name)
+
+for q in range(s):
+    shots = input("(striker_name[s])")
+    print(f"Striker name: {striker_name[s]}")
