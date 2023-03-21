7187d0b5109ea8f946a73699ef3e293268484af5
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-03-21 14:30:55 +0000

    Tue, Mar 21, 2023, 2:30 PM +00:00

--- final/final.py
+++ final/final.py
@@ -2,7 +2,7 @@
 # soccer team's striker roster.
 # (Works under assumption that no two players have equal stats)
 
-def main():
+# def main():
         # Asks how many spots are there
     v = input("How many spots are there?: ")
     v = int(v)
@@ -18,11 +18,12 @@ def main():
         name = input("Striker name: ")
         striker_name.append(name)
 
-    
-    print(f"Striker name: {striker_name[]}")
+    for q in range(s):
+        shots = input("(striker_name[s])")
+    print(f"Striker name: {striker_name[s]}")
 
-    striker_OVR = OVR_calculator(striker_name)
+    # striker_OVR = OVR_calculator(striker_name)
 
 
-def OVR_calculator(striker_name[0]):
-    score = 1
+#def OVR_calculator(striker_name[0]):
+ #   return 1:
