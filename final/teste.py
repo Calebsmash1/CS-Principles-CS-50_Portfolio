e7749685ffd7fee6ad4e94e4acc75b72d9c426ee
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-03-21 14:50:30 +0000

    Tue, Mar 21, 2023, 2:50 PM +00:00

--- final/teste.py
+++ final/teste.py
@@ -19,7 +19,7 @@ for i in range(s):
     shots = input(f"{striker_name[i]} shots?: ")
     striker_name.append(shots)
     #target = input(f"{striker_name[i]} shots on target?: ")
-print(f"{striker_name}")
+print(f"{striker_name[0]}")
 
 #for i in range(s):
     #print(f"{striker_name[i]}")
