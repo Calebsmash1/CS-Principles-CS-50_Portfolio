95a37e1e7bb48f4b5bcd165ee09ac70cc686d222
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-03-21 14:27:30 +0000

    Tue, Mar 21, 2023, 2:27 PM +00:00

--- final/final.py
+++ final/final.py
@@ -3,7 +3,7 @@
 # (Works under assumption that no two players have equal stats)
 
 def main():
-    # Asks how many spots are there
+        # Asks how many spots are there
     v = input("How many spots are there?: ")
     v = int(v)
 
@@ -18,8 +18,11 @@ def main():
         name = input("Striker name: ")
         striker_name.append(name)
 
-    striker_OVR = 
+    
+    print(f"Striker name: {striker_name[]}")
 
-    # print(f"Striker name: {striker_name}")
+    striker_OVR = OVR_calculator(striker_name)
 
-def OVR_calculator(striker_name)
+
+def OVR_calculator(striker_name[0]):
+    score = 1
