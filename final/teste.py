a3d2e74c2a21e645f6955d1759ddbff8538dcb2b
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-03-23 14:34:42 +0000

    Thu, Mar 23, 2023, 2:34 PM +00:00

--- final/teste.py
+++ final/teste.py
@@ -1,37 +1,37 @@
+def main():
 
-# def main():
-# Asks how many spots are there
-v = input("How many spots are there?: ")
-v = int(v)
+    # Asks how many spots are there
+    v = input("How many spots are there?: ")
+    v = int(v)
 
-# Asks how many strikers are trying out
-s = input("How manys strikers are trying out?: ")
-s = int(s)
-if s <= v:
-    print("Recruit them all!")
-    exit()
-striker_name = []
-for i in range(s):
-    name = input("Striker name: ")
-    striker_name.append(name)
+    # Asks how many strikers are trying out
+    s = input("How manys strikers are trying out?: ")
+    s = int(s)
+    if s <= v:
+        print("Recruit them all!")
+        exit()
+    striker_name = []
+    for i in range(s):
+        name = input("Striker name: ")
+        striker_name.append(name)
 
-striker_score = []
-for i in range(s):
-    shots = input(f"{striker_name[i]} shots?: ")
-    shots = int(shots)
-    target = input(f"{striker_name[i]} on target?: ")
-    target = int(target)
-    #if target > shots
-        #print(f"Please provide acurate information)
-    score = target / shots
-    score = score * 100
-    print(f"{score}%")
+    striker_score = []
+    for i in range(s):
+        shots = input(f"{striker_name[i]} shots?: ")
+        shots = int(shots)
+        target = input(f"{striker_name[i]} on target?: ")
+        target = int(target)
+        #if target > shots
+            #print(f"Please provide acurate information)
+        score = target / shots
+        score = score * 100
+        print(f"{score}%")
 
-    striker_score.append(score)
-    #target = input(f"{striker_name[i]} shots on target?: ")
-for i in range (s):
-    print(f"{striker_name[i]}: ", end="")
-    print(f"{striker_score[i]}")
+        striker_score.append(score)
+        #target = input(f"{striker_name[i]} shots on target?: ")
+    for i in range (s):
+        print(f"{striker_name[i]}: ", end="")
+        print(f"{striker_score[i]}")
 
-print(f"{striker_name}")
-print(f"{striker_score}")
+    print(f"{striker_name}")
+    print(f"{striker_score}")
