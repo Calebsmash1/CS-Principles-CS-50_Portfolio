d0c24742345a605bce93f3b0fe40edd41150a245
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-03-24 01:56:15 +0000

    Fri, Mar 24, 2023, 1:56 AM +00:00

--- final/teste.py
+++ final/teste.py
@@ -1,5 +1,8 @@
+
 def main():
     # Asks how many strikers are trying out
+    ovr = []
+
     s = input("How manys strikers are trying out?: ")
     s = int(s)
     if s == 1:
@@ -26,16 +29,23 @@ def main():
 
     striker_rate = []
     for i in range(s):
-        d = input(f"{striker_name[i]} dribbles??: ")
-        d = int(d)
-        s = input(f"{striker_name[i]} on succesful?: ")
-        s = int(s)
-        rate = s / d
+        dri = input(f"{striker_name[i]} dribbles??: ")
+        dri = int(dri)
+        suc = input(f"{striker_name[i]} on succesful?: ")
+        suc = int(suc)
+        rate = suc / dri
         rate = rate * 100
         striker_rate.append(rate)
 
     print(f"{striker_rate}")
     print(f"{striker_name}")
     print(f"{striker_score}")
+
+def ovr_calc(s, striker_rate, striker_score):
+    ovr = []
+    for i in range(s):
+        ovr = ({striker_score[i]} + {striker_rate[i]}) / 2
+        return ovr[i]
+
 
 main()
