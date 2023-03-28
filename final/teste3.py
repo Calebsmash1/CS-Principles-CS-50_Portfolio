0f02487952a0a8ffefd45b4635301ac0511201f7
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-03-28 14:27:51 +0000

    Tue, Mar 28, 2023, 2:27 PM +00:00

--- final/teste3.py
+++ final/teste3.py
@@ -34,45 +34,16 @@ def main():
         accuracy = accuracy * 100
         striker_accuracy.append(accuracy)
 
-    # Asks for striker's dribbling success rate and saves in a list
-    striker_dribble_rate = []
-    for i in range(s):
-        dribbles = input(f"{striker_name[i]} How many dribbles done?: ")
-        dribbles = int(dribbles)
-        successful = input(f"{striker_name[i]} How many were successful?: ")
-        successful = int(successful)
-        if successful > dribbles:
-            print("Please provide accurate information.")
-            exit()
-        rate = successful / dribbles
-        rate = rate * 100
-        striker_dribble_rate.append(rate)
-
-    # Asks for striker's passing success rate and saves in a list
-    striker_pass_rate = []
-    for i in range(s):
-        passes = input(f"{striker_name[i]} How many passes made?: ")
-        passes = int(passes)
-        through = input(f"{striker_name[i]} How many were received?: ")
-        through = int(through)
-        if through > passes:
-            print("Please provide accurate information.")
-            exit()
-        vision = through / passes
-        vision = vision * 100
-        striker_pass_rate.append(vision)
-
     # Calculates player's Overall Rating using their stat averages and saves in a list
     striker_ovr = []
     for i in range(s):
         ovr = 0
-        ovr = ((striker_dribble_rate[i] + striker_accuracy[i] + striker_pass_rate[i]) / 3)
+        ovr = ((striker_dribble_rate[i] + 2))
+        ovr = int(ovr)
         striker_ovr.append(ovr)
 
     print(f"{striker_name}")
     print(f"{striker_accuracy}")
-    print(f"{striker_dribble_rate}")
-    print(f"{striker_pass_rate}")
     print(f"{striker_ovr}")
 
 
@@ -91,8 +62,11 @@ def main():
     print(f"Best Striker is {striker_name[spot]}")
 
 #def accuracy_finder():
+
+
+
 #def dribble_success_rate_finder():
-#def pass_success_rate_finder():
 #def ovr_calculator():
+
 
 main()
