c891c880144baa903d3ddf6e252908976cde4a52
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-04-12 14:38:54 +0000

    Wed, Apr 12, 2023, 2:38 PM +00:00

--- chapter6/favorites4.py
+++ chapter6/favorites4.py
@@ -8,11 +8,9 @@ with open("favorites.csv", "r") as file:
     for row in reader:
         # Remove spaces before and after title, convert all letters to uppercase
         title = row["title"].stripI()upper()
-        # If title is not in the list
-        if not title in titles:
-            # Add to the list
-            titles.append(title)
+        # Add to the list
+        titles.add(title)
 
 # Printing every title
-for title in titles:
+for title in sorted(titles):
     print(title)
