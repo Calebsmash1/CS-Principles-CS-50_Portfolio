294e6f3412364921741a5ba1f08340ecd69c1346
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-04-13 14:36:23 +0000

    Thu, Apr 13, 2023, 2:36 PM +00:00

--- chapter6/search.py
+++ chapter6/search.py
@@ -1,4 +1,13 @@
 import csv
+import re
 
 title = input("Title: ").strip().upper()
 
+
+with open("favorites.csv", "r") as file:
+    reader = csv.DictReader(file)
+
+    for row in reader:
+
+        if re.search(title, row["title"].strip().upper()):
+            counter += 1
