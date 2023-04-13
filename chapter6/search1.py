4c2f1ed75ac05b49aad832de99cc195829fa6ba3
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-04-13 14:40:24 +0000

    Thu, Apr 13, 2023, 2:40 PM +00:00

--- chapter6/search1.py
+++ chapter6/search1.py
@@ -9,8 +9,9 @@ with open("favorites.csv", "r") as file:
     reader = csv.DictReader(file)
 
     for row in reader:
-
         if re.search(title, row["title"].strip().upper()):
+            #print(row)
+            print(row["Timestamp"] + ", " + row["title"])
             counter += 1
 
 print(f"There are {counter} favorite shows that contain the word {title}.")
