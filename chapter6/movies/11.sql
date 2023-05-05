ae31a13c9e7234045d9a8e51d6c8ac24f82206c8
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-05-05 00:36:24 +0000

    Fri, May 5, 2023, 12:36 AM +00:00

--- chapter6/movies/11.sql
+++ chapter6/movies/11.sql
@@ -1,1 +1,1 @@
-SELECT movies.title from people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id JOIN ratings on movies.id = ratings.movie_id WHERE people.name = "Chadwick Boseman" ORDER BY rating DESC LIMIT 5;
+SELECT movies.title from people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id JOIN ratings on movies.id = ratings.movie_id WHERE people.name like "%Chadwick Boseman%" ORDER BY rating DESC LIMIT 5;
