b7f3e81892b949182893fb7f3d279cbdcfe23984
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-05-05 00:30:50 +0000

    Fri, May 5, 2023, 12:30 AM +00:00

--- chapter6/movies/8.sql
+++ chapter6/movies/8.sql
@@ -1,1 +1,1 @@
-SELECT name FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id WHERE movies.title = "Toy Story";
+SELECT name FROM movies, stars, people WHERE movies.id = stars.movie_id AND people.id = stars.person_id AND title = "Toy Story";
