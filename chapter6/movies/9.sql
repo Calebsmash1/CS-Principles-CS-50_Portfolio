8fac636696a72a8b52965a98c70afa3cec9d4057
Calebsmash1 <112988910+Calebsmash1@users.noreply.github.com>
2023-05-05 00:16:25 +0000

    Fri, May 5, 2023, 12:16 AM +00:00

--- chapter6/movies/9.sql
+++ chapter6/movies/9.sql
@@ -1,1 +1,1 @@
-SELECT name from people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id WHERE movies.title = "Toy Story";
+SELECT DISTINCT(people.name) from stars JOIN people ON stars.person_id = people.id JOIN movies ON stars.movie_id = movies.id WHERE movies.year = 2004 ORDER BY birth;
